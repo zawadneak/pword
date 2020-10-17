bae3d15394c11df135d01a1a266682789c39d464
Lucas <lucaszawadneak@gmail.com>
2020-10-17 13:35:25 -0300

    Project structure

---
+++ lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("oi")));
+  }
+}
+
+// class Test extends StatefulWidget{
+//   @override
+//   State<StatefulWidget> createState() {
+//     // TODO: implement createState
+//     throw UnimplementedError();
+//   }
+// }
