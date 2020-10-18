58e171a230be312628b4eb2952b797e0ff065a06
Lucas <lucaszawadneak@gmail.com>
2020-10-18 16:52:12 -0300

    Home structure

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,9 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Home extends StatelessWidget {
+  Function handleClick() {
+    print("oi");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("oi")));
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("Welcome to PWORD"),
+        Text("your password manager & generator"),
+        Padding(
+          padding: EdgeInsets.all(50.0),
+          child: null,
+        ),
+        RaisedButton(
+          onPressed: handleClick,
+          child: Container(
+            child: Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [Text('oi'), Text('oi')],
+              ),
+            ),
+            width: 400.0,
+            height: 200.0,
+          ),
+        ),
+        RaisedButton(
+          onPressed: null,
+          child: Text("Safe"),
+        ),
+        RaisedButton(
+          onPressed: null,
+          child: Text("Settings"),
+        ),
+        Padding(
+          padding: EdgeInsets.all(50.0),
+          child: null,
+        ),
+        Text("v1.0 - 2020"),
+        Text("NeakApps"),
+      ],
+    )));
   }
 }
 
